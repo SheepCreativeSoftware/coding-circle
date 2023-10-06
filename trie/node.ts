7f9a0c380506cc04548fd7516d10ba004b3c5cf6
magraina <marinafcegner@gmail.com>
2023-10-07 01:30:20 +0200

    Added some comments

--- trie/node.ts
+++ trie/node.ts
@@ -13,29 +13,36 @@ class TrieNode {
 		this.end = false;
 	}
 
+	/** Add a new character to the node tree */
 	addNew(char: string): void {
 		this.children[char] = new TrieNode();
 	}
 
-	containsKey(char: string): boolean {
+	/**
+	 * Checks if a node has a child
+	 */
+	hasChild(char: string): boolean {
 		return typeof this.children[char] !== 'undefined';
 	}
 
+	/** Returns the child of the node */
 	getNode(char: string): TrieNode {
 		return this.children[char];
 	}
 
+	/** Sets this node as an end node */
 	setEnd(): void {
 		this.end = true;
 	}
 
+	/** Gets a list of words with the given search string */
 	getWords(word: string): string[] {
 		const results = [] as string[];
 
 		if(this.end) results.push(word);
 
 		for(const [char, childNode] of Object.entries(this.children)) {
-			// ...
+			// Search for more words with the given prefix
 			results.push(...childNode.getWords(word + char));
 		}
 
--- trie/trie.ts
+++ trie/trie.ts
@@ -11,23 +11,25 @@ class Trie {
 		this.rootNode = new TrieNode();
 	}
 
+	/** Adds a word to the Trie */
 	addWord(word: string) {
 		let currentNode = this.rootNode;
 
 		for(let index = 0; index < word.length; index++) {
 			const currentChar = word.charAt(index);
-			if(!currentNode.containsKey(currentChar)) currentNode.addNew(currentChar);
+			if(!currentNode.hasChild(currentChar)) currentNode.addNew(currentChar);
 			currentNode = currentNode.getNode(currentChar);
 		}
 		currentNode.setEnd();
 	}
 
+	/** Searches for words that maches the given word prefix */
 	searchWord(word: string): string[] {
 		let currentNode = this.rootNode;
 
 		for(let index = 0; index < word.length; index++) {
 			const currentChar = word.charAt(index);
-			if(!currentNode.containsKey(currentChar)) return [];
+			if(!currentNode.hasChild(currentChar)) return [];
 			currentNode = currentNode.getNode(currentChar);
 		}
 		return currentNode.getWords(word);
