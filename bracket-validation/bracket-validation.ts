c0e028e053a0a80b0750cb934f246cbc43a80046
magraina <marinafcegner@gmail.com>
2024-11-22 17:35:47 +0100

    refactor: reduced script further

--- bracket-validation/bracket-validation.ts
+++ bracket-validation/bracket-validation.ts
@@ -2,28 +2,27 @@
 const isBalanced = (bracketString: string, openChar: string, closeChar: string): boolean => {
 	let nestingLevel = 0;
 	let availableStars = 0;
-	let possibleOpeningBracket = 0;
 	for (let index = 0; index < bracketString.length; index++) {
 		const char = bracketString[index];
 
-		if (char === openChar) nestingLevel++;
-		else if (char === closeChar) nestingLevel--;
-		else if (char === '*' && nestingLevel === 0) possibleOpeningBracket++;
-		else if (char === '*') availableStars++;
+		if (char === openChar) {
+			nestingLevel++;
+		} else if (char === closeChar) {
+			nestingLevel--;
 
-		// Bracket closed but was not open - try to compensate with a star
-		if (nestingLevel < 0) {
-			if (availableStars > 0) {
-				nestingLevel = 0;
-				availableStars -= 1;
-			} else if (possibleOpeningBracket > 0) {
-				nestingLevel = 0;
-				possibleOpeningBracket -= 1;
+			// Bracket closed but was not open - try to compensate with a star
+			if (nestingLevel < 0) {
+				if (availableStars > 0) {
+					nestingLevel = 0;
+					availableStars -= 1;
+				} else {
+					// Nesting level could not be compensated with a star
+					return false;
+				}
 			}
+		} else if (char === '*') {
+			availableStars++;
 		}
-
-		// Nesting level could not be compensated with a star
-		if (nestingLevel < 0) return false;
 	}
 
 	// Star signs can compensate missing brackets if there is at least the same amount available
